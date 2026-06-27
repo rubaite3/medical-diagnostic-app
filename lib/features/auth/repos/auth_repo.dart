1dd6a2a8e928a8e4d7a242a6ca7a9bffa9136437
Yousef Razzouk <yousefrazouk888@gmail.com>
2026-06-27 06:54:07 +0300

    removed connectivity_plus

--- lib/features/auth/repos/auth_repo.dart
+++ lib/features/auth/repos/auth_repo.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:fpdart/fpdart.dart';
 import 'package:medical_diagnostic_app1/core/api/app_error.dart';
 import 'package:medical_diagnostic_app1/core/api/app_response.dart';
